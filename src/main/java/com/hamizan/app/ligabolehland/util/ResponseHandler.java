3d571dcaab90e5883f2e335f3589979f34976753
hamizannordin <hamizan.nordin@gmail.com>
2021-07-16 00:16:13 +0800

    add response handler

---
+++ src/main/java/com/hamizan/app/ligabolehland/util/ResponseHandler.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hamizan.app.ligabolehland.util;
+
+import com.hamizan.app.ligabolehland.response.BasicResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity.BodyBuilder;
+
+/**
+ *
+ * @author hamizan
+ */
+public class ResponseHandler {
+    
+    private static BodyBuilder response200 = ResponseEntity.status(HttpStatus.OK);
+    private static BodyBuilder response400 = ResponseEntity.status(HttpStatus.BAD_REQUEST);
+    private static BodyBuilder response500 = ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR);
+    
+    public static class OK {
+        
+        public static ResponseEntity build (String body){
+            return response200.body(new BasicResponse(body));
+        }
+    }
+    public static class BAD_REQ {
+    
+        public static ResponseEntity build (String body){
+            return response400.body(new BasicResponse(body));
+        }
+    }
+    public static class SERVER_ERROR {
+    
+        public static ResponseEntity build (String body){
+            return response500.body(new BasicResponse(body));
+        }
+    }
+    
+}
