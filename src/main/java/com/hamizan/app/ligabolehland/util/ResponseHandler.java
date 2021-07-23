11b07cdbec877fa277980e8e8f8dc23d327ebdcd
hamizannordin <hamizan.nordin@gmail.com>
2021-07-23 23:54:26 +0800

    update response handler

--- src/main/java/com/hamizan/app/ligabolehland/util/ResponseHandler.java
+++ src/main/java/com/hamizan/app/ligabolehland/util/ResponseHandler.java
@@ -20,6 +20,7 @@ public class ResponseHandler {
     
     private BodyBuilder response200 = ResponseEntity.status(HttpStatus.OK);
     private BodyBuilder response400 = ResponseEntity.status(HttpStatus.BAD_REQUEST);
+    private BodyBuilder response404 = ResponseEntity.status(HttpStatus.NOT_FOUND);
     private BodyBuilder response500 = ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR);
     
     public ResponseEntity ok (String message, Object body){
@@ -28,6 +29,10 @@ public class ResponseHandler {
     
     public ResponseEntity badRequest (String message, Object body){
         return response400.body(new BasicResponse(message, body));
+    }
+    
+    public ResponseEntity notFound (String message, Object body){
+        return response404.body(new BasicResponse(message, body));
     }
     
     public ResponseEntity serverError (String message, Object body){
